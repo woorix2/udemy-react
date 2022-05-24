d5978a0da33cf342e38161b08425726ee3c494d7
woorix2 <woori8806@naver.com>
2022-05-24 15:39:07 +0900

    Feat: ExpenseForm 양식 변경 초기 'Add New Expense'버튼만 보이고, 클릭 후 'Cancel'버튼이 추가된 기존양식 보이게 함 cancel버튼이나, 양식 입력후 'Add Expense' 버튼 클릭으로 submit하면 다시 'Add New Expense'버튼만 렌더링

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,12 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const onButtonClicked = () => {
+    setButtonClicked(!buttonClicked);
+  };
+
   const titleChangeHandler = (e) => {
     setEnteredTitle(e.target.value);
   };
@@ -26,44 +32,52 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+    setButtonClicked(!buttonClicked);
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+    <div>
+      {buttonClicked === true ? (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button onClick={onButtonClicked}>Cancel</button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
+      ) : (
+        <button onClick={onButtonClicked}>Add New Expense</button>
+      )}
+    </div>
   );
 };
 export default ExpenseForm;
