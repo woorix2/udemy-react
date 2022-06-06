c6ad1fcf73c9f5bc2df068956e14fe29bed63db2
woorix2 <woori8806@naver.com>
2022-06-06 23:00:36 +0900

    Del: 'change title' 버튼 삭제

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -18,7 +18,6 @@ const ExpenseItem = (props) => {
           <h2>{props.title}</h2>
           <div className="expense-item__price">{props.amount}원</div>
         </div>
-        <button onClick={clickHandler}>Change title</button>
       </Card>
     </li>
   );
